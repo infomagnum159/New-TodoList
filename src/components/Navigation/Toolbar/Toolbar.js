faba774f226e3bcdc4999e50af4d76fcfceca292
infomagnum159 <infomagnum159@gmail.com>
2022-07-27 13:40:30 +0600

    Toolbar for NavigationItems

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Toolbar.css';
+import NavigationItems from "../NavigationItems/NavigationItems";
+
+const Toolbar = () => (
+    <header className="Toolbar">
+        <nav>
+            <NavigationItems/>
+        </nav>
+    </header>
+);
+
+export default Toolbar;
